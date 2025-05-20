7bb424dc16dfccf6e8a921239c92b64acc41774c
vansh <singhvanshpreet79@gmail.com>
2025-05-20 10:37:00 +0530

    heelo

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+// import React, { createContext, useState } from 'react'
+// export const datacontext=createContext()
+// function UserContext({children}) {
+
+//     let [input, setInput] = useState("")
+//     let data = {
+//         input,
+//         setInput
+//     }
+//     return (
+//         <div>
+//             <datacontext.Provider value={data}>
+//                 {children}
+//             </datacontext.Provider>
+//         </div>
+//     )
+// }
+
+// export default UserContext
